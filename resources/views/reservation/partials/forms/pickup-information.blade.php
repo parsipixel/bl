394c5524c0907a5f5aefad88b7deda6db4cfc2aa
Ali <nazari.dev@gmail.com>
2019-01-23 13:27:12 +0330

    Change type of date and time inputs

--- resources/views/reservation/partials/forms/pickup-information.blade.php
+++ resources/views/reservation/partials/forms/pickup-information.blade.php
@@ -3,13 +3,13 @@
     <div class="form-group">
         <label for="ReservationPickupDate" class="col-sm-4 col-xs-12 col-form-label text-right">Date of Pick Up</label>
         <div class="col-sm-4 col-xs-12">
-            <input name="pickup_date" class="form-control" type="text" id="ReservationPickupDate"/>
+            <input name="pickup_date" class="datepicker form-control" type="date" id="ReservationPickupDate"/>
         </div>
     </div>
     <div class="form-group">
         <label for="ReservationPickupTime" class="col-sm-4 col-xs-12 col-form-label text-right">Time of Pick Up</label>
         <div class="col-sm-4 col-xs-12">
-            <input name="pickup_time" class="form-control" type="text" id="ReservationPickupTime"/>
+            <input name="pickup_time" class="form-control" type="time" id="ReservationPickupTime"/>
         </div>
     </div>
 </fieldset>
