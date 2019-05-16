2fa45eee5d1e039899793ef68a527d02591a9807
Ali <nazari.dev@gmail.com>
2019-05-16 15:50:24 +0430

    Remove contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,7 +16,7 @@
             you for choosing BLUE SHUTTLE.
         </p>
 
-        <div class="contact-tel col-md-5" style="margin-top: 50px !important;">
+        <div class="contact-tel col-md-12" style="margin-top: 50px !important;">
             <section class="tel "
                      style="color: #003A67; ">
 
@@ -31,47 +31,47 @@
             </section><!-- tel -->
         </div>
 
-        <div class="contact-form col-md-7">
-            <form action="{{ route('post.contact-us') }}" class="form-horizontal clearfix" role="form" id="ContactUsIndexForm" method="post" accept-charset="utf-8">
+        <div class="contact-form col-md-7 hide">
+            {{--<form action="{{ route('post.contact-us') }}" class="form-horizontal clearfix" role="form" id="ContactUsIndexForm" method="post" accept-charset="utf-8">--}}
 
-                {{ csrf_field() }}
-                <fieldset>
-                    <legend>Contact us</legend>
+                {{--{{ csrf_field() }}--}}
+                {{--<fieldset>--}}
+                    {{--<legend>Contact us</legend>--}}
 
-                    <div class="form-group required">
-                        <label for="ContactUsName" class="col-md-5 col-form-label text-right">Name</label>
-                        <div class="col-sm-6 col-xs-12">
-                            <input name="name" class="form-control" type="text" id="ContactUsName" required="required"/>
-                        </div>
-                    </div>
+                    {{--<div class="form-group required">--}}
+                        {{--<label for="ContactUsName" class="col-md-5 col-form-label text-right">Name</label>--}}
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<input name="name" class="form-control" type="text" id="ContactUsName" required="required"/>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
-                    <div class="form-group">
-                        <label for="ContactUsPhone" class="col-md-5 col-form-label text-right">Phone/Mobile Number</label>
-                        <div class="col-sm-6 col-xs-12">
-                            <input name="phone" class="form-control" type="tel" id="ContactUsPhone"/>
-                        </div>
-                    </div>
+                    {{--<div class="form-group">--}}
+                        {{--<label for="ContactUsPhone" class="col-md-5 col-form-label text-right">Phone/Mobile Number</label>--}}
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<input name="phone" class="form-control" type="tel" id="ContactUsPhone"/>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
-                    <div class="form-group required">
-                        <label for="ContactUsEmail" class="col-md-5 col-form-label text-right">Email</label>
-                        <div class="col-sm-6 col-xs-12">
-                            <input name="email" class="form-control" type="email" id="ContactUsEmail" required="required"/>
-                        </div>
-                    </div>
+                    {{--<div class="form-group required">--}}
+                        {{--<label for="ContactUsEmail" class="col-md-5 col-form-label text-right">Email</label>--}}
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<input name="email" class="form-control" type="email" id="ContactUsEmail" required="required"/>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
-                    <div class="form-group required">
-                        <label for="ContactUsMessage" class="col-md-5 col-form-label text-right">Message</label>
-                        <div class="col-sm-6 col-xs-12">
-                            <textarea name="message" class="form-control" cols="30" rows="6" id="ContactUsMessage" required="required"></textarea>
-                        </div>
-                    </div>
+                    {{--<div class="form-group required">--}}
+                        {{--<label for="ContactUsMessage" class="col-md-5 col-form-label text-right">Message</label>--}}
+                        {{--<div class="col-sm-6 col-xs-12">--}}
+                            {{--<textarea name="message" class="form-control" cols="30" rows="6" id="ContactUsMessage" required="required"></textarea>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
-                </fieldset>
+                {{--</fieldset>--}}
 
-                <div class="text-center">
-                    <input value="Submit" class="btn btn-primary" type="submit"/>
-                </div>
-            </form>
+                {{--<div class="text-center">--}}
+                    {{--<input value="Submit" class="btn btn-primary" type="submit"/>--}}
+                {{--</div>--}}
+            {{--</form>--}}
         </div>
     </div>
 @endsection
